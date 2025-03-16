793238b6c8d45357c5540e0cfafa54a9bc9db3bc
Kevin Sante <kksante@gmail.com>
2025-03-15 18:36:59 -0700

    Create Mirror Branches To Track Upstream.

--- createLocalBranches.sh
+++ createLocalBranches.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+### Create Local Branches To Track Upstream Branch
 for branch in $(git branch --all | grep '^\s*remotes' | egrep --invert-match '(:?HEAD|master)$'); do
     git branch --track "${branch##*/}" "$branch"
 done
